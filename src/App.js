f825ee591eea0c9be35ed82815dd38644d404897
Kanako-muff <kanako.design00@gmail.com>
2023-03-10 09:56:14 -0800

    added data.json(forgot to add...)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Card from './components/Card/Card';
+import data from "./assets/data/data.json"
 
 function App() {
+
+  const cardElements = data.map((user) => <Card key={user.fullName} {...user} />);
+
   return (
     <div className="App">
-      <Card />
+      {/* <Card /> */}
+      <div className="card-list">
+        {
+          cardElements
+        }
+      </div>    
     </div>
   );
 }
